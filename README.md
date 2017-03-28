52003dde5926970ed282ec9a80d4b48259bbacd5
Stavros Foteinopoulos <stafot@gmail.com>
2017-03-28 12:26:52 +0300

    Add current public key.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 public key
 ==========
 
-Appends my public key on a host
+Appends my public key/keys on a host
 
 usage
 =====
 
 ```Bash
-wget -O - https://raw.github.com/pmoust/public_key/master/add_key.sh | sh
+curl  https://raw.githubusercontent.com/stafot/public_key/master/add_key.sh | /bin/bash
 ```
--- add_key.sh
+++ add_key.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# My public key
-KEY='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEUqo4hdXCYzsZ4bUBFh00koxFL3Q8mmjzjMs3t/gD22VrJ77xVcjw9arhosMXmS5hqlUKpu1OjEVRpiVQsR+VvXI5RKpuAXPiUThWF5uNAWiHfXSlUYPKfdv3mCrcsEqvmaLgm+W/mqcAHI74/IlRH6CC9anwlvqzMO3WJzGfH2SBwKAdbCJ8V2dGDIq5CWZnGPX/3jFTGGSRsC4o4Y3ArRKSvYJPygFIK1pJj34oWviBCIhKhTOq2zGMOqwHwXmoMhq6NzFLFeqj4dkaHCJ0fQV/wuLyF/PQlev2UFNUINrrHKAUwK+Y+f6hcpAfPJ4yLRVKDKci5eXUtbfxfVIX raccoon';
-
 # Determine key path, warning eval = evil
 HOME=$(eval echo ~${SUDO_USER})
 KEYCHAIN="$HOME/.ssh/authorized_keys"
+GITHUB_USER="stafot"
+CURL=`which curl`
+KEY="$CURL https://github.com/$GITHUB_USER.keys" 
 
 # Append public key to keychain
 echo $KEY >> $KEYCHAIN
